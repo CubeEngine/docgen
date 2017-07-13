61715d3c95bfa331eed7cd656dfb36d093d49589
Stefan Wolf <mail@swolf.me>
2017-07-13 19:50:37 +0200

    adds the first part of the script which creates the docs.

---
+++ gendocs.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+timeout=7200
+scriptDir="$(dirname "$(realpath "$0")")"
+docsDir="${scriptDir}/data/config/cubeengine/modules/docs"
+
+generate_docs() {
+    pushd "${scriptDir}"
+        # start the container
+        echo "starts the containers..."
+        docker-compose up -d
+
+        mkdir -vp "${docsDir}"
+
+        echo "wait for doc files"
+        inotifywait -qqt ${timeout} -e create -e moved_to "${docsDir}"
+
+        # sleep another five seconds to ensure that the files were created completely
+        sleep 5
+
+        # print docker logs
+        echo "Docker logs:"
+        docker-compose logs
+
+        if [ ! -f "${scriptDir}/modules" ]
+        then
+            echo "The docs couldn't be created."
+            exit 1
+        fi
+
+        # clean up environment
+        echo "Clean up environment..."
+        docker-compose rm -fsv
+    popd
+}
+
+echo "Generate Docs..."
+generate_docs
